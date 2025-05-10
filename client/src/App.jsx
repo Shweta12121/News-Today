fceb33bc265dbe7f4f00bd4a9287334e6e15e78e
shweta12121 <shwpand2004@gmail.com>
2025-05-10 19:42:52 +0530

    project frontend and backend completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./components/Header";
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import CountryNews from './components/CountryNews';
+import News from './components/Topheadlines';
+import TopHeadlines from './components/Topheadlines';
 
-function App() {
+
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className='w-full'>
+        <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<News/>} />
+          <Route path="/top-headlines/:category" element={<TopHeadlines/>} />
+          <Route path="/country/:ios" element={<CountryNews/>} />
+        </Routes>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
